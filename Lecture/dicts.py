eae246f18d9419d5d3d10e2fe2f70e818c5f3b4f
Raphael <graphael.dr@gmail.com>
2024-10-24 22:47:01 -0600

    dictionaries

---
+++ Lecture/dicts.py
@@ -0,0 +1,19 @@
+d = { 'a': 1, 'b': 2, 'c': 1, 'd': 2, 'e': 1 }
+
+
+# check if key is in dict
+# print('a' in d) --> True
+# print(1 in d) --> False
+
+# or check if value is in dict
+# print(1 in d.values()) --> True
+
+# for i in d:
+#   print(d[i])
+
+# print(d.values())
+
+# l = list(d.values())
+# print(l)
+
+# print(d.count(10)) --- Not allowed
