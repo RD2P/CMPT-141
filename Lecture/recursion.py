f497215f374581577f15833026187b95629e5ad2
Raphael <graphael.dr@gmail.com>
2024-10-27 23:39:30 -0600

    recursion

--- Lecture/recursion.py
+++ Lecture/recursion.py
@@ -1,5 +1,5 @@
-# anything that can be done with loops can also be done with recursion
 '''
+anything that can be done with loops can also be done with recursion
 base case - smallest possible instance of the problem
 branch 1: base case
 branch 2: recursive case, slightly smaller version of the problem
@@ -19,7 +19,7 @@ def sum_squares(n):
     return 0
   else:
     return n**2 + sum_squares(n-1)
-  
+
 
 # sum all number up to n
 def sum_n(n):
@@ -29,6 +29,28 @@ def sum_n(n):
     return n + sum_n(n-1)
 
 
+# bottles of beer on the wall
+def bottles_of_beer(n):
+  if n <= 0:
+    print("All gone!")
+  else:
+    print(n, "bottles of beer on the wall.")
+    print(n, "bottles of beer.")
+    print("Take one down, pass it around.")
+    print(n-1, "bottles of beer on the wall.\n")
+    bottles_of_beer(n-1)
 
 
-print(sum_n(3))
+# determine if n is even (maximum recursion depth is 1000)
+def is_even(n):
+  if n == 2:
+    return True
+  elif n == 1:
+    return False
+  else:
+    return is_even(n-2)
+
+# recursion limit helps guard against stack overflows
+# import sys
+# print(sys.getrecursionlimit()) # 1000
+
