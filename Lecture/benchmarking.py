f2027f936738e1f87d7b401e92dc069076fa44f6
Raphael <graphael.dr@gmail.com>
2024-10-29 17:02:11 -0600

    benchmarking nested loop

--- Lecture/benchmarking.py
+++ Lecture/benchmarking.py
@@ -3,6 +3,11 @@ from time import perf_counter
 start = perf_counter()
 
 # task
+for i in range(1000):
+  for j in range(1000):
+    a = 3 + 4
 
 duration = perf_counter() - start
+
+print("Task ran in", duration, 'seconds')
 
