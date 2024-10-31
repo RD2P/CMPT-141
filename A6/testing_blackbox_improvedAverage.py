83e1e425cc77811f52d0c831f79c011e4aabd0b9
Raphael <graphael.dr@gmail.com>
2024-10-30 21:09:33 -0600

    dummy function for blackbox testing

---
+++ A6/testing_blackbox_improvedAverage.py
@@ -0,0 +1,16 @@
+def improvedAverage(L):
+    """ returns a boolean value indicating whether the average of the last 10 values in L
+    is greater than the average of the rest of the values in L
+    
+    :params:
+        L: (list) a list of integers or floats
+        
+    :return:
+        (boolean) True if the average of the last 10 values is greater than the average of the rest
+        False if its not
+        None if the length of L is less than 20.
+    """
+    # this is a dummy version of this function. it just always returns False
+    # because we're practicing blackbox testing, we don't get to see the actual function
+    # but this placeholder will let us test if the SYNTAX of the test driver is correct
+    return False
