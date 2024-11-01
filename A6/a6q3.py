28a54d64213efb89ff57d413410908b536b1de2a
Raphael <graphael.dr@gmail.com>
2024-10-31 21:40:31 -0600

    update comments

--- A6/a6q3.py
+++ A6/a6q3.py
@@ -10,7 +10,7 @@ def closest_to_zero(num1, num2, num3):
   Takes 3 values and returns the one that is closest to 0
   Inputs: num1, num2, num3: integers whose distance from 0 will be measured and compared
   Returns:  the integer which is closest to 0;
-            if more than one integer is closest, return the integer that is positive'''
+            if more than one integer is closest, and one is negative and the other positive, return the integer that is positive'''
   
   # Initialize tuple with distane of num1 from 0 and num1 
   closest = (abs(num1), num1)
@@ -20,7 +20,7 @@ def closest_to_zero(num1, num2, num3):
     if abs(i) < closest[0]: # If current argument is closer to 0, update closest tuple
       closest = (abs(i), i)
     elif abs(i) == closest[0]: # If distance is the same as closest distance...
-      # Update closest if the current argument is larger (i.e. closest[1] is negative and i is positive)
+      # Update closest if closest[1] is negative and i is positive
       if i > closest[1]:  
         closest = (abs(i), i)
 
--- A6/a6q3_testdriver.py
+++ A6/a6q3_testdriver.py
@@ -9,7 +9,11 @@
 from a6q3 import closest_to_zero
 
 
-# num1 is closest
+# Assume 3 arguments (num1, num2, num3) are passed to the function and they're all integers
+
+
+# num1 is closest to zero
+# Test when the first if statement in the for loop is never satisfied
 num1, num2, num3 = 1,2,3 
 expected = 1
 result = closest_to_zero(num1, num2, num3)
@@ -17,7 +21,8 @@ if result != expected:
   print("Testing closest_to_zero() with", num1, num2, num3, "   Expected:", expected, " Got: ", result)
 
 
-# num2 is closest
+# num2 is closest to zero
+# Test when first if statement is satisfied on the first iteration of the loop
 num1, num2, num3 = 2, 0, 3 
 expected = 0
 result = closest_to_zero(num1, num2, num3)
@@ -25,15 +30,15 @@ if result != expected:
   print("Testing closest_to_zero() with", num1, num2, num3, "   Expected:", expected, " Got: ", result)
 
 
-# num3 is closest
+# num3 is closest to zero
+# Test when first if statement is satisfied on the second and last iteration of the loop
 num1, num2, num3 = 3, 2, 0
 expected = 0
 result = closest_to_zero(num1, num2, num3)
-if result != expected:
-  print("Testing closest_to_zero() with", num1, num2, num3, "   Expected:", expected, " Got: ", result)
 
 
-# Two arguments are closest, both positive
+# Two arguments are closest - first and third, and both positive
+# Test when the elif is satisfied on the second iteration of the loop, but the nested if statement is not hit
 num1, num2, num3 = 1, 2, 1
 expected = 1
 result = closest_to_zero(num1, num2, num3)
@@ -41,7 +46,17 @@ if result != expected:
   print("Testing closest_to_zero() with", num1, num2, num3, "   Expected:", expected, " Got: ", result)
 
 
+# Two arguments are closest - second and third, and both are positive
+# Test when the if statement is hit on the first iteration, and the elif on the second iteration of the loop is hit
+num1, num2, num3 = 2, 1, 1
+expected = 1
+result = closest_to_zero(num1, num2, num3)
+if result != expected:
+  print("Testing closest_to_zero() with", num1, num2, num3, "   Expected:", expected, " Got: ", result)
+
+
 # Two arguments are closest, both negative
+# Test when the elif is hit but the nested if statement is not hit
 num1, num2, num3 = -1, 2, -1
 expected = -1
 result = closest_to_zero(num1, num2, num3)
@@ -50,6 +65,7 @@ if result != expected:
 
 
 # Two arguments are closest, one negative, one positive
+# Test when the elif is hit and the nested if statement is hit
 num1, num2, num3 = -1, 2, 1
 expected = 1
 result = closest_to_zero(num1, num2, num3)
