4a43aa62a33247ae9e2bc02e7ff98be0c49330d2
Raphael <graphael.dr@gmail.com>
2024-11-02 13:27:10 -0600

    arrays

---
+++ Notes/arrays.py
@@ -0,0 +1,12 @@
+# lists can have different data types
+l = [4, 'a', 5.0]
+# lists can change in length
+l.append('b')
+print(l)
+
+
+# Arrays can't change size
+# Same data type
+# Data stored in contigous block of memory
+
+
