d6424fa458d35cc17ec5c53e51c780ce23abfffc
Raphael <graphael.dr@gmail.com>
2024-11-02 15:18:20 -0600

    creating numpy arrays

--- Notes/arrays.py
+++ Notes/arrays.py
@@ -5,8 +5,44 @@ l.append('b')
 print(l)
 
 
-# Arrays can't change size
+# Arrays
+# Can't change size
 # Same data type
 # Data stored in contigous block of memory
+# Good for images, number of data does not change, same data type, need fast operations
+# NUMPY library for working with arrays
+
+# numpy has ND array
+# pandas is built on top of numpy 
 
 
+import numpy as np
+
+
+# create arrays with lists or tuples
+A = np.array([1,3,4,5])
+print(A) # not separated by commas
+B = np.array((6,7,8,9))
+print(B)
+
+
+# 2D arrays (has to be rectangular shape)
+AA = np.array([[1,2],[3,4],[5,6]])
+print(AA) # separated into rows
+BB = np.array(((1,2),(3,4)))
+print(BB)
+
+
+# creating zeros
+z = np.zeros(5)
+print(z) # default data type is float
+# create 2d zeros by giving list or tuple
+zz = np.zeros([3,4])
+print(zz)
+tt = np.zeros((2,2))
+print(tt)
+
+
+# ones
+o = np.ones(4)
+print(o)
