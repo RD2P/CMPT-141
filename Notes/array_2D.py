7a461312829217d426acd035378c1c99d58532d9
Raphael <graphael.dr@gmail.com>
2024-11-05 17:14:17 -0600

    2d practice

---
+++ Notes/array_2D.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+turbines = [ 
+  [ 2.5, 2.2, 3.4],
+  [ 3.5, 2.9, 4.4],
+  [ 9.1, 3.4, 6.5]]
+
+# 2d array
+
+A = np.array(turbines)
+print(A, '\n')
+
+# dimensions of wind farm
+print('dimension: ', A.ndim)
+print('shape', A.shape)
+
+# 3rd row, 1st col  
+print(A[2,0]) # or
+print(A[2][0])
+
+# average
+print('ave', A.mean()) # average of all items
+print()
+
+# average of subset first 2 of first 2 rows
+subset = A[:2,:2]
+print(subset)
+print(np.sum(subset)/subset.size)
+print(np.average(subset))
+
+# sum everything
+print(np.sum(A))
