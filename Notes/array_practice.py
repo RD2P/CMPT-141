cbc9d41a558bbd8efecead8f6f3ee8bada8ed661
Raphael <graphael.dr@gmail.com>
2024-11-05 20:52:18 -0600

    iterating over array, creating array from a range

--- Notes/array_practice.py
+++ Notes/array_practice.py
@@ -1,35 +1,44 @@
 import numpy as np
 
-coffee_shops = [x for x in range(1,100)]
+# coffee_shops = [x for x in range(1,100)]
+
+# CA = np.array(coffee_shops)
+
+# # size
+# print(CA.size)
+
+# # largest element
+# print(CA[CA.size - 1]) # just do CA[-1]
+
+# # first 5 elements
+# for i in range(6):
+#   print(CA[i])
+
+# # len on arrays gets only length of first dimension
+# A = np.array([[10,9],[3,4],[8,1]])
+# print(len(A))
+
+# print()
+
+# # sorting on arrays, sorts the last dimension
+# A.sort()
+# print(A)
+
+# print("B")
+# B = np.array([[[4,2],[10,3]],[[4,2],[10,3]]])
+# B.sort()
+# print(B)
 
 
-CA = np.array(coffee_shops)
+# # np operations on scalars
+# print(np.sum([2,3,4]))
 
-# size
-print(CA.size)
+# A = np.array([[10,9],[3,4],[8,1]])
+# print()
+# print(A)
+# for i in A:
+#   for j in i:
+#     print(j)
 
-# largest element
-print(CA[CA.size - 1]) # just do CA[-1]
-
-# first 5 elements
-for i in range(6):
-  print(CA[i])
-
-# len on arrays gets only length of first dimension
-A = np.array([[10,9],[3,4],[8,1]])
-print(len(A))
-
-print()
-
-# sorting on arrays, sorts the last dimension
-A.sort()
-print(A)
-
-print("B")
-B = np.array([[[4,2],[10,3]],[[4,2],[10,3]]])
-B.sort()
-print(B)
-
-
-# np operations on scalars
-print(np.sum([2,3,4]))
+A = np.array(range(1,11,2))
+print(A + 1)
