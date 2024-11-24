66f07757d6747d3661387dd70741833761df3789
Raphael <graphael.dr@gmail.com>
2024-11-23 23:50:27 -0600

    insertion sort

--- Notes/sorting/insertion_sort.py
+++ Notes/sorting/insertion_sort.py
@@ -1,11 +1,11 @@
 l = [3,2,4,5,1,8,7,9]
 
+def sort(L):
+  for i in range(1,len(L)):
+    j = i
+    while L[j-1] > L[j] and j > 0:
+      L[j-1], L[j] = L[j], L[j-1]
+      j -= 1
+  return L
 
-
-# def sort(l):
-#   t = []
-#   for i in l:
-#     print(i)
-
-# list insertion:
-l.insert(2, 'a') # index, item
+print(sort(l))
