7d74da8629469ec410dd08e9f4e1b544b7c1adf8
Raphael <graphael.dr@gmail.com>
2024-11-26 09:44:15 -0600

    insertion sort with new sequence

--- Sorting/insertion_sort.py
+++ Sorting/insertion_sort.py
@@ -1,6 +1,15 @@
 l = [3,2,4,5,1,8,7,9]
 
 def sort(L):
+  S = []
+  for n in L:
+    i = 0
+    while i < len(S) and n >= S[i]:
+      i += 1
+    S.insert(i, n)
+  return S
+
+def sort_in_place(L):
   for i in range(1,len(L)):
     j = i
     while L[j-1] > L[j] and j > 0:
