480fc3600339db0166d28f67eb3cc125672a288b
Raphael <graphael.dr@gmail.com>
2024-11-28 17:52:21 -0600

    simplify, don't need the else

--- Sorting/merge_sort.py
+++ Sorting/merge_sort.py
@@ -19,11 +19,11 @@ def merge(S1, S2):
 def merge_sort(L):
   if len(L) <= 1:
     return L
-  else:
-    mid = len(L) // 2
-    S1 = L[:mid]
-    S2 = L[mid:]
-    return merge(merge_sort(S1), merge_sort(S2))
+
+  mid = len(L) // 2
+  S1 = L[:mid]
+  S2 = L[mid:]
+  return merge(merge_sort(S1), merge_sort(S2))
 
 l = [6,5,4,3,2,1,4,5,6,7,8]
 print(merge_sort(l))
