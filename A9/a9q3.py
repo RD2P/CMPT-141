333273ce6f3fa7c43cb41821553a27a314eee1b1
Raphael <graphael.dr@gmail.com>
2024-11-28 19:52:10 -0600

    list pokemons in continent and count number of pokemon for each type

--- A9/a9q3.py
+++ A9/a9q3.py
@@ -46,6 +46,47 @@ def  find_continents(db):
   return res
 
 
-db =  read_pokedata('pokemonLocations.txt')
+def  pokemon_in_continent(db, continent):
+  '''Creates a list of pokemon names that can be found in the continent
+  Input:
+    dict of pokemon info with pokmon names as the key and each value is a dict in the form:
+        - name (str): The Pokemon's name.
+        - type (str): The Pokemon's type.
+        - locations (list): A list of strings representing locations where the Pokemon can be found.
+    continent (str): The name of the continent
+  Returns: a list of pokemon names that can be found in the continent'''
+  res = []
+  for record in db.values():
+    if continent in record["locations"]:
+      res.append(record["name"])
+  return res
 
-print(find_continents(db))
+
+def count_types(db, names):
+  '''Creates a dictionary of pokemon types and the number of pokemon of each type in the list
+  Input:
+    dict of pokemon info with pokmon names as the key and each value is a dict in the form:
+        - name (str): The Pokemon's name.
+        - type (str): The Pokemon's type.
+        - locations (list): A list of strings representing locations where the Pokemon can be found.
+    names (list): A list of pokemon names
+  Returns: a dictionary of pokemon types and the number of pokemon of each type in the list'''
+  res = {}
+  for name in names:
+    type = db[name]["type"]
+    if type in res:
+      res[type] += 1
+    else:
+      res[type] = 1
+  return res
+
+
+db =  read_pokedata('pokemonLocations.txt') # create the database
+continents = find_continents(db) # create list of continents
+
+# iterate over the continents to print the write up about each continent
+for continent in continents:
+  pokemons = pokemon_in_continent(db, continent)
+  print(f"{continent}: {len(pokemons)} total pokemon")
+  print(count_types(db, pokemons))
+  print()
